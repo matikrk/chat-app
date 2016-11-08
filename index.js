12c7d590ac8f108dda5c3ae936b77bc35f0a0611
matikrk <matikrk@gmail.com>
2016-11-08 12:48:11 +0100

    init

---
+++ index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/static/index.html');
+});
+
+io.on('connection', function(socket) {
+    var user = 'user' + ~~(Math.random() * 100);
+
+    console.log(`user ${user} connected`);
+
+    socket.on('disconnect', function() {
+        console.log(`user ${user} disconnected`);
+    });
+});
+
+var port = 8080;
+http.listen(port, function() {
+    console.log(`listening on *:${port}`);
+});
