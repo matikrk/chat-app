f3b2192b79c47ab2e4c62192827399031ae4bcca
matikrk <matikrk@gmail.com>
2016-11-09 11:57:36 +0100

    refactor html file

--- index.js
+++ index.js
@@ -4,19 +4,39 @@ var http = require('http').Server(app);
 
 var path = require('path');
 
+// Use express-ws to enable web sockets.
+require('express-ws')(app);
+
 var io = require('socket.io')(http);
 
 app.use(express.static(path.join(__dirname, 'public')));
 //app.get('/', function(req, res) {
 //    res.sendFile(__dirname + '/static/index.html');
 //});
+
+
+
+// A simple echo service.
+app.ws('/chat', function(ws) {
+    ws.on('open', function() {
+        ws.send('open');
+    });
+    ws.on('message', function(msg) {
+        ws.send('response: ' + msg);
+    });
+    ws.on('close', function() {
+        ws.send('close');
+    });
+});
+
+
 io.on('connection', function(socket) {
     var user = 'user' + ~~(Math.random() * 100);
 
     console.log(`user ${user} connected`);
 
     socket.on('chat message', function(msg) {
-      io.emit('chat message', msg);
+        io.emit('chat message', msg);
     });
 
     socket.on('disconnect', function() {
