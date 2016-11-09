e5cfccc9603f5ebb8ab0adac505c0074e47af950
matikrk <matikrk@gmail.com>
2016-11-09 14:17:34 +0100

    change to websockets

--- index.js
+++ index.js
@@ -2,47 +2,49 @@ var express = require('express')
 var app = express();
 var http = require('http').Server(app);
 
-var path = require('path');
-
 // Use express-ws to enable web sockets.
 require('express-ws')(app);
 
-var io = require('socket.io')(http);
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 //app.get('/', function(req, res) {
 //    res.sendFile(__dirname + '/static/index.html');
 //});
 
+app.get('/wsLocation.js', function(req, res) {
+    var location = '\'localhost\'';
+    var script = `window.wsLocation = ${location};`;
+    res.set('Content-Type', 'text/javascript');
+    res.send(script)
+});
 
-
-// A simple echo service.
+var connectedWs = {};
+var broadcastMsg = function(msg) {
+        Object.values(connectedWs).forEach(ws => ws.send(msg));
+    }
+    // A simple echo service.
 app.ws('/chat', function(ws) {
-    ws.on('open', function() {
-        ws.send('open');
-    });
+    var user = (+new Date() + ~~(Math.random() * 1000)).toString(36);
+
+    connectedWs[user] = ws;
+    ws.send(user + 'open');
+    console.log(`user ${user} connected, ${Object.keys(connectedWs).length} users connected`);
+
     ws.on('message', function(msg) {
-        ws.send('response: ' + msg);
+        broadcastMsg(msg);
+        //  ws.send(user + 'response: ' + msg);
     });
     ws.on('close', function() {
-        ws.send('close');
+        console.log(`user ${user} close connection, ${Object.keys(connectedWs).length} users connected`);
+        delete connectedWs[user]
     });
+
 });
 
-
-io.on('connection', function(socket) {
-    var user = 'user' + ~~(Math.random() * 100);
-
-    console.log(`user ${user} connected`);
-
-    socket.on('chat message', function(msg) {
-        io.emit('chat message', msg);
-    });
-
-    socket.on('disconnect', function() {
-        console.log(`user ${user} disconnected`);
-    });
+// Start the websocket server
+var wsServer = app.listen('65080', function() {
+    console.log('Websocket server listening on port %s', wsServer.address().port);
 });
+
 
 var port = process.env.PORT || 8080;
 http.listen(port, function() {
