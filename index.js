cf7cfde24f658f510088f6b6477d6201254c6385
matikrk <matikrk@gmail.com>
2016-11-09 14:58:53 +0100

    add gcloud support

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
 const express = require('express')
 const app = express();
 const http = require('http').Server(app);
+var request = require('request');
 
 // Use express-ws to enable web sockets.
 require('express-ws')(app);
 
 app.use(express.static('public'));
+
+
+
 //app.get('/', function(req, res) {
 //    res.sendFile(__dirname + '/static/index.html');
 //});
 
+
+
+
+// [START external_ip]
+// In order to use websockets on App Engine, you need to connect directly to
+// application instance using the instance's public external IP. This IP can
+// be obtained from the metadata server.
+var METADATA_NETWORK_INTERFACE_URL = 'http://metadata/computeMetadata/v1/' +
+    '/instance/network-interfaces/0/access-configs/0/external-ip';
+
+function getExternalIp(cb) {
+    var options = {
+        url: METADATA_NETWORK_INTERFACE_URL,
+        headers: {
+            'Metadata-Flavor': 'Google'
+        }
+    };
+
+    request(options, function(err, resp, body) {
+        if (err || resp.statusCode !== 200) {
+            console.log('Error while talking to metadata server, assuming localhost');
+            return cb('localhost');
+        }
+        return cb(body);
+    });
+}
+// [END external_ip]
+
 app.get('/wsLocation.js', function(req, res) {
-    const location = '\'localhost\'';
-    const script = `window.wsLocation = ${location};`;
-    res.set('Content-Type', 'text/javascript');
-    res.send(script)
+    //  const location = '\'localhost\'';
+    getExternalIp(function(location) {
+        const script = `window.wsLocation = \'${location}\';`;
+        res.set('Content-Type', 'text/javascript');
+        res.send(script)
+    });
 });
 
 const connectedWs = {};
